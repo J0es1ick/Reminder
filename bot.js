d6bfbbbd2720922e0e586bf3c31760e50450ca8a
J0es1ick <mrjoeslick@gmail.com>
2024-10-07 22:38:26 +0300

    update ignored files

--- bot.js
+++
@@ -1,6 +0,0 @@
-const TelegramBot = require("node-telegram-bot-api");
-
-const token = "7280997507:AAFqm189AJXhDKWVtRdcB51sEbq9BrGQEUo";
-const bot = new TelegramBot(token, { polling: true });
-
-module.exports = bot;
