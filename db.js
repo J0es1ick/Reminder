1ff51198558ec966c47caecfe55c5a6ef26932c8
J0es1ick <mrjoeslick@gmail.com>
2024-10-07 22:47:54 +0300

    update ignored files

--- db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("ReminderDB", "postgres", "12345", {
-  host: "localhost",
-  dialect: "postgres",
-  port: 5000,
-});
-
-module.exports = sequelize;
